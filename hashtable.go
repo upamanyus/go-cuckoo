fdc02be03068a825b9b054ed5b1269ccc81e78cf
Upamanyu Sharma <upamanyu@mit.edu>
2021-06-13 15:26:23 -0400

    Fixed hashing to prevent cuckoo cycles; works ok now at high load factor

--- hashtable.go
+++ hashtable.go
@@ -1,9 +1,5 @@
 package cuckoo
 
-import (
-	"github.com/tchajed/goose/machine"
-)
-
 type KVPair struct {
 	k uint64
 	v uint64
@@ -22,8 +18,8 @@ type Bucket struct {
 func (b *Bucket) tryGet(k uint64, v *uint64) bool {
 	var ret bool
 	ret = false
-	for _, kv := range b.kvpairs {
-		if kv.k == k {
+	for j, kv := range b.kvpairs {
+		if kv.k == k && b.occupied[j] {
 			*v = kv.v
 			ret = true
 			break
@@ -61,7 +57,10 @@ func (m *CuckooMap) index1(k uint64) uint64 {
 
 func (m *CuckooMap) index2(k uint64) uint64 {
 	// from cuckoohash_map.hh
-	nonzero_tag := (k & 0xff) + uint64(1)
+	t1 := k ^ (k>>32)&0xffffffff
+	t2 := t1 ^ (t1>>16)&0xffff
+	partial := t2 ^ (t2>>8)&0xff
+	nonzero_tag := partial + uint64(1)
 	return (k ^ (nonzero_tag * 0xc6a4a7935bd1e995)) & m.mask
 }
 
@@ -136,21 +135,28 @@ func (m *CuckooMap) Insert(k uint64, v uint64) uint64 {
 	triesLeft := 1
 
 	m.lock_two(i1, i2)
-	for m.tryInsert(i1, i2, k, v) == INSERT_FAIL {
+	for {
+		r := m.tryInsert(i1, i2, k, v)
 		m.unlock_two(i1, i2)
+		if r != INSERT_FAIL {
+			return r
+		}
+
 		if triesLeft == 0 {
 			return INSERT_FAIL
 		}
 		triesLeft--
 
 		p := m.cuckoo_search(i1, i2)
+		if len(p) == 0 {
+			m.lock_two(i1, i2)
+			continue
+		}
 		if !m.cuckoo_move(p) {
 			panic("unable to move along cuckoo path")
 		}
 		m.lock_two(i1, i2)
 	}
-	m.unlock_two(i1, i2)
-	return INSERT_OK
 }
 
 func (m *CuckooMap) lock_two(i1 uint64, i2 uint64) {
@@ -196,8 +202,8 @@ type cuckooSearchEntry struct {
 
 func (m *CuckooMap) cuckoo_search(i1 uint64, i2 uint64) cuckooPath {
 	q := make([]cuckooSearchEntry, 0, 256)
-	q = append(q, cuckooSearchEntry{path:nil, i:i1})
-	q = append(q, cuckooSearchEntry{path:nil, i:i2})
+	q = append(q, cuckooSearchEntry{path: nil, i: i1})
+	q = append(q, cuckooSearchEntry{path: nil, i: i2})
 	for len(q) > 0 {
 		e := q[0]
 		q = q[1:]
@@ -206,26 +212,25 @@ func (m *CuckooMap) cuckoo_search(i1 uint64, i2 uint64) cuckooPath {
 		for j, o := range m.buckets[e.i].occupied {
 			if !o {
 				hash := uint64(0) // no key there; this hash value is never used
-				e.path = append(e.path, cuckooPathRecord{bucket:e.i, slot:uint64(j), hash:hash})
+				e.path = append(e.path, cuckooPathRecord{bucket: e.i, slot: uint64(j), hash: hash})
 				m.locks[lockind(e.i)].Unlock()
 				return e.path
+			} else if len(e.path) < 9 {
+				slotToEvict := uint64(j)
+				hash := m.buckets[e.i].kvpairs[slotToEvict].k // XXX: identity hash fn
+				altI := m.index1(m.buckets[e.i].kvpairs[slotToEvict].k)
+				if altI == e.i {
+					altI = m.index2(m.buckets[e.i].kvpairs[slotToEvict].k)
+				}
+
+				newPath := make([]cuckooPathRecord, 0, len(e.path)+1)
+				newPath = append(newPath, e.path...)
+				newPath = append(newPath, cuckooPathRecord{bucket: e.i, slot: slotToEvict, hash: hash})
+
+				newE := cuckooSearchEntry{path: newPath, i: altI}
+				q = append(q, newE)
 			}
 		}
-
-		// XXX: this picks a random slot to try evicting.  As a result, this is
-		// basically DFS with two starting nodes. Should probably try doing BFS,
-		// or if we really want DFS, should optimize the data structures in this
-		// function for DFS.
-		slotToEvict := machine.RandomUint64() % SLOTS_PER_BUCKET
-		hash := m.buckets[e.i].kvpairs[slotToEvict].k // XXX: identity hash fn
-		altI := m.index2(m.buckets[e.i].kvpairs[slotToEvict].k)
-
-		newPath := make([]cuckooPathRecord, 0, len(e.path)+1)
-		newPath = append(newPath, e.path...)
-		newPath = append(newPath, cuckooPathRecord{bucket:e.i, slot:slotToEvict, hash:hash})
-
-		newE := cuckooSearchEntry{path: newPath, i: altI}
-		q = append(q, newE)
 
 		m.locks[lockind(e.i)].Unlock()
 	}
@@ -239,13 +244,13 @@ func (m *CuckooMap) cuckoo_move(path cuckooPath) bool {
 	var j uint64
 	j = uint64(len(path)) - 1
 	for j > 0 {
-		fromI := path[j - 1].bucket
+		fromI := path[j-1].bucket
 		toI := path[j].bucket
 		m.lock_two(fromI, toI)
 
-		fromSlot := path[j - 1].slot
+		fromSlot := path[j-1].slot
 		toSlot := path[j].slot
-		hash := path[j - 1].hash
+		hash := path[j-1].hash
 		j = j - 1
 
 		if !m.buckets[fromI].occupied[fromSlot] {
--- test_test.go
+++ test_test.go
@@ -1,6 +1,7 @@
 package cuckoo
 
 import (
+	"fmt"
 	"testing"
 	"github.com/tchajed/goose/machine"
 )
@@ -9,23 +10,46 @@ func checkEquivalence(m map[uint64]uint64, c *CuckooMap) bool {
 	var vtemp uint64
 	for k, v := range m {
 		if !c.Get(k, &vtemp) {
-			// k should be in c, but it isn't; error
-			return false
+			panic("Missing key")
 		} else if vtemp != v {
-			// value doesn't match
-			return false
+			fmt.Printf("%d -> %d instead of %d", k, vtemp, v)
+			panic("Value doesn't match")
 		}
 	}
 	return true
 }
 
 func TestMapSingleThreaded(t *testing.T) {
-	c := MakeCuckooMap(16)
+	c := MakeCuckooMap(15)
 	m := make(map[uint64]uint64)
 
-	N := 70000
+	N := 127000
 	for i := 0; i < N; i++ {
 		k := machine.RandomUint64()
+		v := machine.RandomUint64()
+		m[k] = v
+		r := c.Insert(k, v)
+		if r == INSERT_FAIL {
+			t.Fatalf("CuckooMap.Insert() failed")
+		} else if r == INSERT_DUP {
+			t.Fatalf("CuckooMap.Insert() duplicate")
+		}
+	}
+	if !checkEquivalence(m, c) {
+		t.Fatalf("Maps not equivalent")
+	}
+}
+
+
+func TestMapSequential(t *testing.T) {
+	hashpower := uint64(18)
+	c := MakeCuckooMap(hashpower)
+	m := make(map[uint64]uint64)
+
+	// when inserting sequentially, can get to 100% load factor!
+	N := SLOTS_PER_BUCKET * (1 << hashpower)
+	for i := 0; i < N; i++ {
+		k := uint64(i)
 		v := machine.RandomUint64()
 		m[k] = v
 		r := c.Insert(k, v)
